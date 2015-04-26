3d36ce8b059116aa7b224ee31648e42bb2f52997
Craig Greenberg <csgreenberg@blake.cs.umass.edu>
2015-04-26 07:50:46 -0400

    tracking master.sh and run.sh

--- master.sh
+++ master.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-#dimList="10 50 100"
-#lrList="1.0 0.1 0.01"
-#regList="0.1 0.01 0.001"
-#negativeList="1 10 20"
-dimList="50"
-lrList="0.1"
-regList="0.01"
-negativeList="1"
+dimList="10 50 100"
+lrList="1.0 0.1 0.01 0.001"
+regList="1.0 0.1 0.01 0.001"
+negativeList="1 5 10 20"
+
+#dimList="100"
+#lrList="1.0"
+#regList="0.1"
+#negativeList="1"
+
 threadList="20"
 for dim in $dimList
 do
@@ -18,8 +20,7 @@ for neg in $negativeList
 do
 for thread in $threadList
 do
-qsub -S /bin/sh -l mem_token=${2}G -l mem_free=${2}G run.sh $dim $lr $reg $neg $thread $1 $2
-#./run.sh $dim $lr $reg $neg $thread
+qsub -S /bin/sh -l mem_token=30G -l mem_free=30G run_freebaseCluewebClassifier.sh $dim $lr $reg $neg $thread $1
 done
 done
 done
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-cd /iesl/canvas/arvind/universalSchema/factorie
+cd /iesl/canvas/csgreenberg/c0d3/universalSchema_fork/univSchema
 if [ ! -e CP.hack ]; then
     echo "Run make_cp.sh script first"
     exit    
 fi
 
 classpath=`cat CP.hack`
-app="java -Xmx${7}g -cp ${classpath} cc.factorie.app.nlp.embeddings.UniversalSchema"
-$app --train=../data//train --dev=../data//dev --test=../data//test --size $1 --rate $2 --regularizer $3 --negative $4 --threads $5 --options $6  --epochs 30 --frequency 10 --writeOutput false 
+app="java -Xmx30g -cp ${classpath} cc.factorie.app.nlp.embeddings.UniversalSchema"
+$app --train=/iesl/canvas/proj/processedClueweb12/freebase/msr/msrFreebaseTrain.v0.tsv --dev=/iesl/canvas/proj/processedClueweb12/freebase/msr/msrFreebaseDev70.v0.tsv --test=/iesl/canvas/proj/processedClueweb12/freebase/msr/msrFreebaseTest70.v0.tsv --testRelationsFile=/iesl/canvas/proj/processedClueweb12/freebase/msr/release_v0.4/70Types --size $1 --rate $2 --regularizer $3 --negative $4 --threads $5 --options 3  --epochs 30 --frequency 10 --tree $6

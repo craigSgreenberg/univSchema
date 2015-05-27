156e7a3c2fa89a15747eecc60f72fe65e1f76a95
cSg <csgreenberg@cs.umass.edu>
2015-05-27 16:06:35 -0400

    initial commit of train/dev/test split code

---
+++ create_freebase_train_dev_test_split.py
@@ -0,0 +1,44 @@
+
+from __future__ import division
+
+from collections import defaultdict
+from collections import Counter
+
+
+import random
+random.seed(42)
+
+def get_type2freebaseid(filepath, sep='\t'):
+    type2freebaseid = defaultdict()
+    i = 0
+    with open(filepath) as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            #/m/010bvf       %%base%%type_ontology%%inanimate        1
+            freebaseid, ftype, val = line.split(sep)
+            assert val == '1'
+            type2freebaseid[ftype] = freebaseid
+            i += 1
+            if i > 10000:
+                break
+    return type2freebaseid
+
+def main():
+    % choose m of the n most common types
+    n = 1000
+    m = 100
+    freebase_type_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/msr/msrFreebaseAll.v0.tsv'
+    train_filepath = ''
+    dev_filepath = ''
+    test_filepath = ''
+    type2freebaseid = get_type2freebaseid(freebase_type_filepath)
+    counts = [(len(v), k) for k,v in type2freebaseid.iteritems()]
+    counts.sort(reverse=True)
+    selected_types = random.sample([t for (_, t) in counts[:n]],m)
+    
+
+
+if __name__ == '__main__':
+    main()

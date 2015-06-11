a132792c7cd43d46a8c6a6b5368e88a85ff7e1b8
cSg <csgreenberg@cs.umass.edu>
2015-06-11 12:55:57 -0400

    updated dataset selection to ignore types that are actually freebase ids or freebase application parameters.

--- create_freebase_train_dev_test_split.py
+++ create_freebase_train_dev_test_split.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 import random
 random.seed(42)
 
-def get_type2freebaseid(filepath, sep='\t'):
+def get_type2freebaseid(filepath, sep='\t', type_filter=lambda x: True):
     type2freebaseid = defaultdict(list)
     with open(filepath) as f:
         for line in f:
@@ -14,7 +14,8 @@ def get_type2freebaseid(filepath, sep='\t'):
             #/m/010bvf       %%base%%type_ontology%%inanimate        1
             freebaseid, ftype, val = line.split(sep)
             assert val == '1'
-            type2freebaseid[ftype].append(freebaseid)
+            if type_filter(ftype):
+                type2freebaseid[ftype].append(freebaseid)
     return type2freebaseid
 
 def split_train_dev_test(type2freebaseid, selected_types, train_per=.6, dev_per=.1):
@@ -52,20 +53,28 @@ def reformat_pat_and_arvinds_entity_file(infilepath='/iesl/canvas/proj/processed
                     outf.write('%s\t%s\t1\n'%(entity, t))
 
 def write_train_dev_test_splits():
-    # choose m of the n most common types
+    """
+    Choose m of the n most common types for tests (the rest go into train only),
+    split into train/dev/test."""
+    # Set filepaths
+    # replaced MSR file with Pat and Arvind's entity file...
+    # freebase_type_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/msr/msrFreebaseAll.v0.tsv'
+    freebase_type_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/iesl/entity_to_fbtypes.tsv'
+    train_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/freebaseTrain.v2.tsv'
+    dev_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/freebaseDev.v2.tsv'
+    test_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/freebaseTest.v2.tsv'
+    # Set parameters
     n = 1000
     m = 100
-    # replaced MSR file with Pat and Arvind's entity file...
-    #freebase_type_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/msr/msrFreebaseAll.v0.tsv'
-    freebase_type_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/iesl/entity_to_fbtypes.tsv'
-    train_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/freebaseTrain.v1.tsv'
-    dev_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/freebaseDev.v1.tsv'
-    test_filepath = '/iesl/canvas/proj/processedClueweb12/freebase/freebaseTest.v1.tsv'
-    type2freebaseid = get_type2freebaseid(freebase_type_filepath)
+    # Select test types
+    type_filter = lambda x: x[:3] != "/m/" and x[:10] != "/freebase/"
+    type2freebaseid = get_type2freebaseid(freebase_type_filepath, type_filter=type_filter)
     counts = [(len(v), k) for k,v in type2freebaseid.iteritems()]
     counts.sort(reverse=True)
     selected_types = random.sample([t for (_, t) in counts[:n]],m)
+    # Split into train/dev/test
     train, dev, test = split_train_dev_test(type2freebaseid, selected_types)
+    # Write splits to files
     write_to_file(train, train_filepath)
     write_to_file(dev, dev_filepath)
     write_to_file(test, test_filepath)
